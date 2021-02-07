026e0863a8589119d7501388ceabdae4b4aac04c
mike.zounaropoulos <mike.zounaropoulos@foodtecsolutions.com>
2021-02-07 08:16:25 -0500

    refactor services. Closes #15

--- pet-clinic-data/src/main/java/itzamic/springframework/petclinic/services/VetInterface.java
+++ pet-clinic-data/src/main/java/itzamic/springframework/petclinic/services/VetInterface.java
@@ -2,12 +2,5 @@ package itzamic.springframework.petclinic.services;
 
 import itzamic.springframework.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetInterface {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetInterface extends CrudService<Vet, Long> {
 }
