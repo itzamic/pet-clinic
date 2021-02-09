2f1b0b542f805cfadc4e069e169cd3e072a1a7ce
mike.zounaropoulos <mike.zounaropoulos@foodtecsolutions.com>
2021-02-09 10:36:58 -0500

    Create Specialty Map Service Impl. Closes #52

---
+++ pet-clinic-data/src/main/java/itzamic/springframework/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package itzamic.springframework.petclinic.services;
+
+import itzamic.springframework.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
