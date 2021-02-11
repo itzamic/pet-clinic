2288092aca63bb6433d4d569992bc4c6b566876c
mike.zounaropoulos <mike.zounaropoulos@foodtecsolutions.com>
2021-02-11 16:10:04 -0500

    Create Spring Data JPA Service for Owners. Closes #61

--- pet-clinic-data/src/main/java/itzamic/springframework/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/itzamic/springframework/petclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import itzamic.springframework.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastname(String lastname);
 }
