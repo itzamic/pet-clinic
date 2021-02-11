f286650b14f153582d61c6ae31e0228ea968313e
mike.zounaropoulos <mike.zounaropoulos@foodtecsolutions.com>
2021-02-11 16:57:32 -0500

    Update Profiles to use Spring Data JPA Services. Closes #88

--- pet-clinic-data/src/main/java/itzamic/springframework/petclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-data/src/main/java/itzamic/springframework/petclinic/services/map/SpecialtyMapService.java
@@ -2,11 +2,13 @@ package itzamic.springframework.petclinic.services.map;
 
 import itzamic.springframework.petclinic.model.Specialty;
 import itzamic.springframework.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
     @Override
     public Set<Specialty> findAll() {
