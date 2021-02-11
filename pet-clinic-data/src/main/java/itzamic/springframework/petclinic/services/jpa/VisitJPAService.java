506c940bb2061c2f0b34a26779f1f319838a12a2
mike.zounaropoulos <mike.zounaropoulos@foodtecsolutions.com>
2021-02-11 16:48:23 -0500

    Create JPA visit service. Closes #87

---
+++ pet-clinic-data/src/main/java/itzamic/springframework/petclinic/services/jpa/VisitJPAService.java
@@ -0,0 +1,46 @@
+package itzamic.springframework.petclinic.services.jpa;
+
+import itzamic.springframework.petclinic.model.Visit;
+import itzamic.springframework.petclinic.repositories.VisitRepository;
+import itzamic.springframework.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@Profile("jpa")
+public class VisitJPAService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitJPAService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return StreamSupport.stream(visitRepository.findAll().spliterator(), false).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
--- pet-clinic-web/src/main/java/itzamic/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/itzamic/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package itzamic.springframework.petclinic.bootstrap;
 
 import itzamic.springframework.petclinic.model.*;
-import itzamic.springframework.petclinic.services.OwnerService;
-import itzamic.springframework.petclinic.services.PetTypeService;
-import itzamic.springframework.petclinic.services.SpecialtyService;
-import itzamic.springframework.petclinic.services.VetService;
+import itzamic.springframework.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -73,13 +72,19 @@ public class DataLoader implements CommandLineRunner {
         owner2.setCity("Miami");
         owner2.setTelephone("1234567890");
 
-        Pet FionaPet = new Pet();
-        FionaPet.setPetType(savedCatPetType);
-        FionaPet.setOwner(owner2);
-        FionaPet.setName("Mirko");
-        FionaPet.setBirthDate(LocalDate.now());
-        owner1.getPets().add(FionaPet);
+        Pet fionaPet = new Pet();
+        fionaPet.setPetType(savedCatPetType);
+        fionaPet.setOwner(owner2);
+        fionaPet.setName("Mirko");
+        fionaPet.setBirthDate(LocalDate.now());
+        owner1.getPets().add(fionaPet);
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionaPet);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners....");
 
