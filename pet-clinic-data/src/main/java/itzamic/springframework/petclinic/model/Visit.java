47b54797f7aba9133e62824a66eebe21b171953a
mike.zounaropoulos <mike.zounaropoulos@foodtecsolutions.com>
2021-02-11 15:46:42 -0500

    Create Visit Entity. Closes #59

--- pet-clinic-data/src/main/java/itzamic/springframework/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/itzamic/springframework/petclinic/model/Visit.java
@@ -2,11 +2,18 @@ package itzamic.springframework.petclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 }
