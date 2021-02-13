a2e77e830d6ebfd8dc84ab3fe4ecd443941fb513
mike.zounaropoulos <mike.zounaropoulos@foodtecsolutions.com>
2021-02-13 07:52:21 -0500

    fix broken tests

--- pet-clinic-web/src/test/java/izamic/springframework/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/izamic/springframework/petclinic/PetClinicApplicationTests.java
@@ -2,10 +2,12 @@ package izamic.springframework.petclinic;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
+@SpringBootConfiguration
 @SpringBootTest
 class PetClinicApplicationTests {
 
