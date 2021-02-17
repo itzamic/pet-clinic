d187a14a9a1dafb737264838c5b41cabdda5e7b2
mike.zounaropoulos <mike.zounaropoulos@foodtecsolutions.com>
2021-02-17 14:02:06 -0500

    Write test for empty find owner last name by to return all. Closes #119

--- pet-clinic-web/src/test/java/itzamic/springframework/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/itzamic/springframework/petclinic/controllers/OwnerControllerTest.java
@@ -82,4 +82,16 @@ class OwnerControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/owners/1"));
     }
+
+    @Test
+    void processFindFormReturnAll() throws Exception {
+        when(ownerService.findAllByLastnameLike(anyString()))
+                .thenReturn(List.of(Owner.builder().id(1L).build(), Owner.builder().id(2L).build(), Owner.builder().id(3L).build()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerList"))
+                .andExpect(model().attribute("selections", hasSize(3)));
+    }
+
 }
