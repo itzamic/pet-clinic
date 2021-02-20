4a663465f6c0aff10831170f3432cf4f406f12a3
mike.zounaropoulos <mike.zounaropoulos@foodtecsolutions.com>
2021-02-20 05:42:14 -0500

    Implement Vets JSON Endpoint. Closes #128

--- pet-clinic-web/src/main/java/itzamic/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/itzamic/springframework/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package itzamic.springframework.petclinic.controllers;
 
+import itzamic.springframework.petclinic.model.Vet;
 import itzamic.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/vets")
@@ -18,5 +23,10 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
